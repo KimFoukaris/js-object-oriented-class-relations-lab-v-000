a20215a7685c5104ca60b767cf6742771815d626
Kim Foukaris <kimfoukaris@outlook.com>
2018-07-01 18:08:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,63 @@
+let store = {drivers: [], trips: [], passengers: []}
+
+let driverId = 0;
+class Driver {
+  constructor (name) {
+    this.id = ++driverId;
+    this.name = name;
+    store.drivers.push(this);
+  }
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId === this.id;
+    })
+  }
+  passengers() {
+    return this.trips().map(function (trip) {
+      return store.passengers.find(function (el) {
+        return el.id === trip.passengerId;
+      })
+    })
+  }
+}
+
+let passengerId = 0;
+class Passenger {
+  constructor (name) {
+    this.id = ++passengerId;
+    this.name = name;
+    store.passengers.push(this);
+  }
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId === this.id;
+    })
+  }
+  drivers() {
+    return this.trips().map(function (trip) {
+      return store.drivers.find(function (el) {
+        return el.id === trip.driverId;
+      })
+    })
+  }
+}
+
+let tripId = 0;
+class Trip {
+  constructor (driver, passenger) {
+    this.id = ++tripId;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    store.trips.push(this);
+  }
+  driver() {
+    return store.drivers.find(function(driver) {
+      return driver.id === this.driverId;
+    }.bind(this))
+    }
+  passenger() {
+    return store.passengers.find(function(passenger) {
+      return passenger.id === this.passengerId;
+    }.bind(this))
+  }
+}
